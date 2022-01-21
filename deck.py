61ad1a3b0ade1ff54eca6b0bd1d4d41d1705b6b4
marcusBYUI <ojo18002@byui.edu>
2022-01-21 12:43:00 +0100

    added comments, spaces and and clear function

--- deck.py
+++ deck.py
@@ -1,13 +1,22 @@
 import random
 from art import logo
+import os
 
 
 class deck:
+    """The deck class initializes with an array to store the numbers used in the game class
+    
+    number module generates a random number from the range of 1 and 13 then it adds it to the numbers listed
+    if the generated number is already in the list, it would generate a new one
+    
+    """
 
     def __init__(self):
         self.numbers = []
 
     def number(self):
+        """Generates a random number from the range of 1 and 13 then it adds it to the numbers listed
+            if the generated number is already in the list, it would generate a new one"""
         self.current_number = random.randint(1, 13)
         while self.current_number in self.numbers:
             self.current_number = random.randint(1, 13)
@@ -15,6 +24,7 @@ class deck:
 
 
 class Hilo:
+    """The Hilo class handles the game play, it initializes with the users score and the deck class """
     def __init__(self):
         self.score = 300
         self.deck = deck()
@@ -22,30 +32,37 @@ class Hilo:
         self.is_playing = True
 
     def display(self):
-        # print(logo)
+        """The display module prints the current card to the user. it takes the first item and
+            in the array declared in the deck class"""
         self.current_number = self.deck.numbers[0]
         print(f"The card is: {self.current_number}")
 
     def user_input(self):
+        """The user_input module is loop, asks the user's input, verifies the input then proceeds to call the
+            other modules required in the gameplay"""
         while self.is_playing:
             letters = ["l", "h", "q"]
             self.display()
             self.user = input("Higher or lower? (h/l or q for quit): ").lower()
+            
             while self.user not in letters:
                 print("Please enter h, l or q")
-                self.user = input(
-                    "Higher or lower? (h/l or q for quit): ").lower()
+                self.user = input("Higher or lower? (h/l or q for quit): ").lower()
 
             self.second_number()
             self.compare()
 
     def second_number(self):
+        """The second_number module calls the number module in the deck class which adds a new number to the list of
+            The new number is now index 1 and takend as the next number then displayed to the user"""
         self.deck.number()
         self.next_number = self.deck.numbers[1]
         if self.user != "q":
             print(f"Next card is: {self.next_number}")
 
     def compare(self):
+        """The compare module compares the user input with the numbers and then computes the score.
+            The first number in the numbers list us popped and a check is made to see if the game should continue"""
 
         if self.user == "q":
             self.is_playing = False
@@ -65,6 +82,7 @@ class Hilo:
 
         self.deck.numbers.pop(0)
         print(f"Your score is: {self.score}")
+        print()
         if self.score <= 0:
             self.is_playing = False
             self.play_again = input("Game Over. Play again? (y/n): ")
@@ -74,6 +92,7 @@ class Hilo:
 
 def main():
     hilo = Hilo()
+    os.system('cls||clear')
     print(logo)
     hilo.user_input()
 
