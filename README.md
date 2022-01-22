7c11baf77f9662925c1396eea2495146595adff3
JulieAntezana <jantezana@byui.edu>
2022-01-22 03:28:02 -0600

    Updated README file

--- README.md
+++ README.md
@@ -1,60 +1,67 @@
 # cse210-02
+
 Repository for CSE 210 Team-J-12pm game design for week 3.
 
-Hilo Specification
+HiLo Game
 
-Life is not a matter of holding good cards, 
+Life is not a matter of holding good cards,
 but sometimes, playing a poor hand well.
+--Jack London
 
-Jack London 
-
-Overview
+## Overview
 
 Hilo is a game in which the player guesses if the next card drawn by the dealer will be higher or lower than the previous one. Points are won or lost based on whether or not the player guessed correctly.
 
-Rules
-Hilo is played according to the following rules.
+## Rules
 
+Hilo is played according to the following rules:
 The player starts the game with 300 points.
-
 Individual cards are represented as a number from 1 to 13.
-
 The current card is displayed.
-
 The player guesses if the next one will be higher or lower.
-
 The the next card is displayed.
-
 The player earns 100 points if they guessed correctly.
-
 The player loses 75 points if they guessed incorrectly.
-
 If a player reaches 0 points the game is over.
+If a player has more than 0 points they may decide to quit or keep going.
+If a player decides to quit the game is over.
 
-If a player has more than 0 points they decide if they want to keep playing.
+## Getting Started
 
-If a player decides not to play again the game is over.
+---
 
-![image](https://user-images.githubusercontent.com/90766037/149841251-3ccd3438-7d71-4266-a7e4-aa8b0d9a0155.png)
+Make sure you have Python 3.8.0 or newer installed and running on your machine. Open a terminal and browse to the project's root folder. Start the program by running the following command.
 
-Requirements
+```
+python3 HiLo
+```
 
--The program must also meet the following requirements.
+You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the
+project folder. Select the main module inside the cse210-02 folder and click the "run" button.
 
--The program must include a README file.
+## Project Structure
 
--The program must include class and method comments.
+---
 
--The program must have at least two classes.
+The project files and folders are organized as follows:
 
--The program must remain true to game play described in the overview.
+```
+root(cse210-02)          (project root folder)
+  +-- deck.py            (source code for game)
+  +-- art.py             (art file for logo)
+  +-- README.md          (general info)
+```
 
-Have Some Fun
+## Required Technologies
 
-Have some fun by enhancing the game any way you like. A few ideas are as follows.
+---
 
--Enhanced input validation.
+- Python 3.8.0
 
--Enhanced game play and game over messages.
+## Authors
 
--Enhanced game display, e.g. card suits
+team-j-12pm:
+Marcus Ayodeji Ojo-Osasere
+Rune Larsen
+Lukas Nakale
+Julie Antezana
--- deck.py
+++ deck.py
@@ -1,13 +1,22 @@
 import random
 from art import logo
+import os
 
 
 class deck:
+    """The deck class initializes with an array to store the numbers used in the game class
+
+    number module generates a random number from the range of 1 and 13 then it adds it to the numbers listed
+    if the generated number is already in the list, it would generate a new one
+
+    """
 
     def __init__(self):
         self.numbers = []
 
     def number(self):
+        """Generates a random number from the range of 1 and 13 then it adds it to the numbers listed
+            if the generated number is already in the list, it would generate a new one"""
         self.current_number = random.randint(1, 13)
         while self.current_number in self.numbers:
             self.current_number = random.randint(1, 13)
@@ -15,6 +24,8 @@ class deck:
 
 
 class Hilo:
+    """The Hilo class handles the game play, it initializes with the users score and the deck class """
+
     def __init__(self):
         self.score = 300
         self.deck = deck()
@@ -22,36 +33,44 @@ class Hilo:
         self.is_playing = True
 
     def display(self):
-        # print(logo)
+        """The display module prints the current card to the user. it takes the first item and
+            in the array declared in the deck class"""
         self.current_number = self.deck.numbers[0]
         print(f"The card is: {self.current_number}")
 
     def user_input(self):
+        """The user_input module is loop, asks the user's input, verifies the input then proceeds to call the
+            other modules required in the gameplay"""
         while self.is_playing:
             letters = ["l", "h", "q"]
             self.display()
             self.user = input("Higher or lower? (h/l or q for quit): ").lower()
+
             while self.user not in letters:
                 print("Please enter h, l or q")
                 self.user = input(
                     "Higher or lower? (h/l or q for quit): ").lower()
+            if self.user == "q":
+                print(f"Thanks for playing, Your score is: {self.score}")
+                self.is_playing = False
+                print()
+                return
 
             self.second_number()
             self.compare()
 
     def second_number(self):
+        """The second_number module calls the number module in the deck class which adds a new number to the list of
+            The new number is now index 1 and takend as the next number then displayed to the user"""
         self.deck.number()
         self.next_number = self.deck.numbers[1]
-        if self.user != "q":
-            print(f"Next card is: {self.next_number}")
+        print(f"Next card is: {self.next_number}")
 
     def compare(self):
+        """The compare module compares the user input with the numbers and then computes the score.
+            The first number in the numbers list us popped and a check is made to see if the game should continue"""
 
-        if self.user == "q":
-            self.is_playing = False
-            print(f"Your score is: {self.score}")
-
-        elif self.user == "h":
+        if self.user == "h":
             if self.current_number < self.next_number:
                 self.score += 100
             else:
@@ -65,6 +84,7 @@ class Hilo:
 
         self.deck.numbers.pop(0)
         print(f"Your score is: {self.score}")
+        print()
         if self.score <= 0:
             self.is_playing = False
             self.play_again = input("Game Over. Play again? (y/n): ")
@@ -74,6 +94,7 @@ class Hilo:
 
 def main():
     hilo = Hilo()
+    os.system('cls||clear')
     print(logo)
     hilo.user_input()
 
@@ -92,9 +113,6 @@ init
 display function:
 takes a card from the deck class and then prints its
 it asks user input
-
 compare funtion:
 new number from the deck class 
-
-
 """
